96c0a7a6d1904a2a9e5e74191656f6e18cfa7bf9
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2025-11-26 13:29:45 +0100

    feat(application): implement custom mediator pattern
    Add custom implementation of mediator pattern with:
    - IRequest and IRequestHandler interfaces
    - ISender for sending requests
    - Automatic handler registration via reflection

--- RaceTrack.Application/Extensions/ServiceCollectionExtensions.cs
+++ RaceTrack.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using RaceTrack.Application.Messaging;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace RaceTrack.Application.Extensions
@@ -9,6 +12,10 @@ namespace RaceTrack.Application.Extensions
     {
         public static void AddApplication(this IServiceCollection services)
         {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediator(assembly);
+            services.AddValidatorsFromAssembly(assembly);
 
         }
     }
---
+++ RaceTrack.Application/Messaging/ISender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.Application.Messaging
+{
+    public interface ISender
+    {
+        Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    }
+}
